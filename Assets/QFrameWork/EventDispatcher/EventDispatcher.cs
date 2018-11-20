c871cf99595bd5b1e0409dd9359716bcd94d8638
Anndy <476919612@qq.com>
2018-11-20 09:56:44 +0800

    修改BaseView

--- Assets/QFrameWork/EventDispatcher/EventDispatcher.cs
+++ Assets/QFrameWork/EventDispatcher/EventDispatcher.cs
@@ -62,7 +62,7 @@ namespace QFrameWork
 
         public void Load()
         {
-            UnityEngine.Debug.Log("Load");
+
         }
     }
 }
--- Assets/QFrameWork/UI/BaseView.cs
+++ Assets/QFrameWork/UI/BaseView.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
 
 namespace QFrameWork
 {
@@ -57,6 +59,53 @@ namespace QFrameWork
             return baseView as T;
         }
 
+        /// <summary>
+        /// 设置按钮的监听事件，仅存在当前事件
+        /// </summary>
+        protected Button SetButtonListener(string componentPath, UnityAction callback)
+        {
+            var tmp = FindButton(componentPath);
+            tmp.onClick.RemoveAllListeners();
+            tmp.onClick.AddListener(callback);
+            return tmp;
+        }
+
+        /// <summary>
+        /// 设置文本内容，此方法会调用获取脚本的方法，如果是频繁需要赋值的对象，建议FindText缓存对象之后继续进行操作
+        /// </summary>
+        protected Text SetText(string componentPath, string text)
+        {
+            var tmp = FindText(componentPath);
+            CheckComponent(tmp, componentPath);
+            tmp.text = text;
+            return tmp;
+        }
+
+        void CheckComponent(object target, string componentPath)
+        {
+            if (target == null)
+                throw new NullReferenceException(string.Format("Component: {0} is not exists!", componentPath));
+        }
+
+        protected Button FindButton(string componentPath)
+        {
+            var tmp = FindChild<Button>(componentPath);
+            CheckComponent(tmp, componentPath);
+            return tmp;
+        }
+
+        protected Text FindText(string componentPath)
+        {
+            var tmp = FindChild<Text>(componentPath);
+            CheckComponent(tmp, componentPath);
+            return tmp;
+        }
+
+        protected T FindChild<T>(string componentName) where T : Component
+        {
+            return transform.Find(componentName).GetComponent<T>();
+        }
+
         public GameObject gameObject
         {
             get { return this.m_gameObject; }
@@ -79,12 +128,12 @@ namespace QFrameWork
 
         public virtual void OnEnter()
         {
-
+            this.gameObject.SetActive(true);
         }
 
         public virtual void OnExit()
         {
-
+            this.gameObject.SetActive(false);
         }
 
         public virtual void OnPause()
